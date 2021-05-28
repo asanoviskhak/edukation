bbe4a1d4ff2dedd68645b353f4ce5cbbdaff8ad5
Iskhak Asanov <asanoviskhak@unist.ac.kr>
2021-05-29 04:49:40 +0900

    :tada: Fixed notion api fetching data

--- src/sections/landing1/FeaturedJobs.js
+++ src/sections/landing1/FeaturedJobs.js
@@ -6,25 +6,20 @@ import imgF3 from "../../assets/image/l1/png/feature-brand-3.png";
 import imgF4 from "../../assets/image/l1/png/feature-brand-4.png";
 import imgF5 from "../../assets/image/l1/png/feature-brand-5.png";
 import imgF6 from "../../assets/image/l1/png/feature-brand-6.png";
-import {getContent} from '../../utils/notiondb'
+import {getContent} from '../../../lib/api'
 const axios = require('axios');
 
 
   
-const FeaturedJobs = ({content}) => {
-  // const [content, setContent] = useState({})
+export default function FeaturedJobs ({content, name}) {
+  const [data, setData] = useState({})
   // const NOTION_API_SECRET="secret_Fm2IDh3zQXNfd1cTAQBW24VXpIuIcfEnqtF5S7TSH9I"
   // const NOTION_DATABASE = "78b6c8d24ebe439bbb5a32d8815693ca"
-  // useEffect(() => {
-  //   const url = `https://api.notion.com/v1/databases/${NOTION_DATABASE}`
-  //   axios.get(url, {
-  //       headers: {
-  //         "Authorization": `Bearer ${NOTION_API_SECRET}`,
-  //         "Notion-Version": "2021-05-13",
-  //       }
-  //   }).then((response)=>{console.log(response)}).catch((err)=>{console.log(err)});
-   
-  // }, [])
+  useEffect(() => {
+    if (content){
+      console.log(content)
+    }
+  }, [])
 
   return (
     <>
@@ -36,7 +31,7 @@ const FeaturedJobs = ({content}) => {
             {/* <!-- Section Title --> */}
             <div className="col-12 col-xl-6 col-lg-6">
               <div className="text-center text-lg-left mb-13 mb-lg-0">
-                <h2 className="font-size-9 font-weight-bold">Hello!</h2>
+                <h2 className="font-size-9 font-weight-bold">{name}</h2>
               </div>
             </div>
             {/* <!-- Section Button --> */}
@@ -53,7 +48,7 @@ const FeaturedJobs = ({content}) => {
           </div>
           {/* <!-- End Section Top --> */}
           <div className="row justify-content-center">
-          {content.map(uni => (
+          {content ? content.map(uni => (
                 <div
                 className="col-12 col-lg-4 col-md-6 px-xxl-7"
                 data-aos="fade-up"
@@ -127,7 +122,84 @@ const FeaturedJobs = ({content}) => {
                 </div>
                 {/* <!-- End Feature One --> */}
               </div>
-            ))}
+            ))
+            :
+            <div
+                className="col-12 col-lg-4 col-md-6 px-xxl-7"
+                data-aos="fade-up"
+                data-aos-duration="800"
+              >
+                {/* <!-- Start Feature One --> */}
+                
+                <div className="bg-white px-8 pt-9 pb-7 rounded-4 mb-9 feature-cardOne-adjustments">
+                  <div className="d-block mb-7">
+                    <Link href="/#">
+                      <a>
+                        <img src={imgF1} alt="" />
+                      </a>
+                    </Link>
+                  </div>
+                  <Link href="/#">
+                    <a className="font-size-3 d-block mb-0 text-gray">
+                      Google INC
+                    </a>
+                  </Link>
+                  <h2 className="mt-n4">
+                    <Link href="/#">
+                      <a className="font-size-7 text-black-2 font-weight-bold mb-4">
+                        UNIST
+                      </a>
+                    </Link>
+                  </h2>
+                  <ul className="list-unstyled mb-1 card-tag-list">
+                    <li>
+                      <Link href="/#">
+                        <a className="bg-regent-opacity-15 text-denim font-size-3 rounded-3">
+                          <i className="icon icon-pin-3 mr-2 font-weight-bold"></i>{" "}
+                          Berlyn
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/#">
+                        <a className="bg-regent-opacity-15 text-orange font-size-3 rounded-3">
+                          <i className="fa fa-briefcase mr-2 font-weight-bold"></i>{" "}
+                          Full-time
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/#">
+                        <a className="bg-regent-opacity-15 text-eastern font-size-3 rounded-3">
+                          <i className="fa fa-dollar-sign mr-2 font-weight-bold"></i>{" "}
+                          80K-90K
+                        </a>
+                      </Link>
+                    </li>
+                  </ul>
+                  <p className="mb-7 font-size-4 text-gray">
+                    We are looking for Enrollment Advisors who are looking to take
+                    30-35 appointments per week. All leads are pre-scheduled.
+                  </p>
+                  <div className="card-btn-group">
+                    <Link href="/#">
+                      <a className="btn btn-green text-uppercase btn-medium rounded-3">
+                        Apply Now
+                      </a>
+                    </Link>
+                    <Link href="/#">
+                      <a className="btn btn-outline-mercury text-black-2 text-uppercase btn-medium rounded-3">
+                        <i className="icon icon-bookmark-2 font-weight-bold mr-4 font-size-4"></i>{" "}
+                        Save it
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+                {/* <!-- End Feature One --> */}
+              </div>
+            
+            
+            }
           </div>
         </div>
       </div>
@@ -135,16 +207,19 @@ const FeaturedJobs = ({content}) => {
   );
 };
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const content = await getContent();
-  
+  if (!content) {
+    return {
+      notFound: true,
+    }
+  }
   return {
     props: {
       content,
+      name:"Hello World!"
       // database: process.env.NOTION_DATABASE
     },
     revalidate: 3600
   };
 }
-
-export default FeaturedJobs;
